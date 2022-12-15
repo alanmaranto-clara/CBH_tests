b2a4eaf6c58c4db843f54006c1343f45281565d9
alanmaranto-clara <alan.arostegui@clara.team>
2022-12-14 20:58:37 -0600

    test: update tests

--- cbh-take-home-project/dpk.test.js
+++ cbh-take-home-project/dpk.test.js
@@ -7,9 +7,32 @@ describe("deterministicPartitionKey", () => {
   });
   it("Return the candidate with the partition event key", () => {
     const event = {
-      partitionKey: "1"
-    }
+      partitionKey: "1",
+    };
     expect(deterministicPartitionKey(event)).toBe("1");
   });
-  //  it("Return the candidate with the partitionKey");
+  it("Return the candidate without the partitionKey", () => {
+    const event = {};
+    const expectedValue = deterministicPartitionKey(event);
+    expect(expectedValue && typeof expectedValue === "string").toBe(true);
+  });
+  it("Return the candidate stringyfied", () => {
+    const event = {
+      partitionKey: 1,
+    };
+    const expectedValue = deterministicPartitionKey(event);
+    expect(expectedValue && typeof expectedValue === "string").toBe(true);
+  });
+  it("Return the candidate with trivialpartition key", () => {
+    const event = {
+      partitionKey: "100",
+    };
+    expect(deterministicPartitionKey(event)).toBe("100");
+  });
+  it("Return the candidate hashed", () => {
+    const event = {
+      partitionKey: 257,
+    };
+    expect(deterministicPartitionKey(event)).toBe("257");
+  });
 });
